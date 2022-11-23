a69a7f883ec8918324b8993bdb0b1dd99a371700
adsetyawan <aridwis37@gmail.com>
2022-11-23 10:01:28 +0700

    commit

--- MODUL3 DWI/config/delete.php
+++ MODUL3 DWI/config/delete.php
@@ -8,7 +8,7 @@ $sql = "DELETE FROM showroom_dwi_table WHERE id_mobil = $id";
 if (mysqli_query($connector, $sql)) {
   header("location: ../pages/List-Dwi.php");
 } else {
-  echo "Gagal";
+  echo "Tidak berhasil";
 }
 
 ?>
