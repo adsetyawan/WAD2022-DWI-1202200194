752ce53694fedeb896f355717252a70d19ee01d3
adsetyawan <aridwis37@gmail.com>
2022-11-27 16:23:06 +0700

    commit

--- MODUL3 DWI/config/insert.php
+++ MODUL3 DWI/config/insert.php
@@ -10,12 +10,15 @@ $status = $_POST['status'];
 $gambar = $_FILES['gambar']['name'];
 $target = "../asset/images/";
 
-if (!$img) {
-  return false;
+if (move_uploaded_file($_FILES['gambar']['tmp_name'], $target . $gambar)) {
+  $sql = "INSERT INTO showroom_dwi_table (nama_mobil, pemilik_mobil, merk_mobil, tanggal_beli, deskripsi, foto_mobil, status_pembayaran) VALUES ('$namamobil', '$pemilik', '$merk', '$tanggalbeli', '$desc', '$gambar', '$status')";
+  if (mysqli_query($koneksi, $sql)) {
+    header("location: ../pages/List-Dwi.php");
+  } else {
+    echo "Gagal";
+  }
+} else {
+  echo "Gagal";
 }
-$query = "INSERT INTO showroom_dwi_table (nama_mobil, pemilik_mobil, merk_mobil, tanggal_beli, deskripsi, foto_mobil, status_pembayaran,belum lunas')";
 
-mysqli_query($db, $query);
-
-return mysqli_affected_rows($db)
 ?>
